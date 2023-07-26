22b735d311f6acbb2a6e8e04b1477273f9b8d115
arnavbansal25 <arnavbansal.8232@gmail.com>
2023-07-27 00:14:16 +0530

    hero section + about section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/no-unknown-property */
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
-    <div>Computers</div>
-  )
-}
+    <>
+      <mesh>
+        <hemisphereLight intensity={0.15} groundColor="black" />
+        <pointLight intensity={1} />
+        <spotLight
+          position={[-20, 50, 10]}
+          angle={0.12}
+          penumbra={1}
+          intensity={1}
+          castShadow
+          shadow-mapSize={1024}
+        />
+        <primitive
+          object={computer.scene}
+          scale={isMobile ? 0.7 : 0.75}
+          position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+          rotation={[-0.01, -0.2, -0.1]}
+        />
+      </mesh>
+    </>
+  );
+};
 
-export default Computers
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500[x])");
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }} // camera position is imp, it suggests from what angle exactly is the end user looking at smthg (x, y, z)...fov : field of view (how wide the fov is going to be)
+      gl={{ preserveDrawingBuffer: true }} // this needs to there to properly render our computer model
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls // these controls allows us to move the model left/right/up/down
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2} // this allows to only rotate around a specific angle and not in all directions
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
